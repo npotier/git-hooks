199827a22a0e824d793269a67af05b8bbfc635c6
Nicolas Potier <nicolas.potier@acseo-conseil.fr>
2016-04-06 12:30:19 +0200

    Create phpmd-last-commit.sh

---
+++ phpmd-last-commit.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+TMP_STAGING="/tmp/staging"
+PHPCS_FILE_PATTERN="\.(php|phtml)$"
+
+FILES=$(git diff-index --name-only --cached --diff-filter=ACMR HEAD^1 --)
+
+if [ "$FILES" == "" ]; then
+    exit 0
+fi
+
+# create temporary copy of staging area
+if [ -e "$TMP_STAGING" ]; then
+    rm -rf $TMP_STAGING
+fi
+mkdir $TMP_STAGING
+
+# match files against whitelist
+FILES_TO_CHECK=""
+for FILE in $FILES
+do
+    echo "$FILE" | egrep -q "$PHPCS_FILE_PATTERN"
+    RETVAL=$?
+    if [ "$RETVAL" -eq "0" ]
+    then
+        FILES_TO_CHECK="$FILES_TO_CHECK $FILE"
+    fi
+done
+
+if [ "$FILES_TO_CHECK" == "" ]; then
+    exit 0
+fi
+
+# Copy contents of staged version of files to temporary staging area
+# because we only want the staged version that will be commited and not
+# the version in the working directory
+STAGED_FILES=""
+for FILE in $FILES_TO_CHECK
+do
+  ID=$(git diff-index --cached HEAD^1 $FILE | cut -d " " -f4)
+
+  # create staged version of file in temporary staging area with the same
+  # path as the original file so that the phpcs ignore filters can be applied
+  mkdir -p "$TMP_STAGING/$(dirname $FILE)"
+  git cat-file blob $ID > "$TMP_STAGING/$FILE"
+  STAGED_FILES="$STAGED_FILES $TMP_STAGING/$FILE"
+done
+
+phpmd $TMP_STAGING text codesize,unusedcode,naming
